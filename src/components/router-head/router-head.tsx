46d0b2138dba7ccd7943b9f7309c58c7244dceae
jose-donato <zmcdonato@gmail.com>
2023-01-01 18:41:26 +0000

    fix: some more updates

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,5 +1,5 @@
-import { component$ } from '@builder.io/qwik';
-import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
@@ -11,10 +11,62 @@ export const RouterHead = component$(() => {
   return (
     <>
       <title>{head.title}</title>
+      <meta name="title" content="José Donato" />
+      <meta
+        name="description"
+        content="I'm a Fullstack Engineer from Portugal with a master's degree in Informatics Security and bachelor in Computer Science. Currently working remotely at OpenBB."
+      />
+      <meta
+        property="og:image"
+        content="https://jose-donato.deno.dev/og_image.png"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://jose-donato.deno.dev" />
+      <meta property="og:title" content="José Donato" />
+      <meta
+        property="og:description"
+        content="I'm a Fullstack Engineer from Portugal with a master's degree in Informatics Security and bachelor in Computer Science. Currently working remotely at OpenBB."
+      />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://jose-donato.deno.dev" />
+      <meta property="twitter:title" content="José Donato" />
+      <meta
+        property="twitter:image"
+        content="https://jose-donato.deno.dev/og_image.png"
+      />
+      <meta
+        property="twitter:description"
+        content="I'm a Fullstack Engineer from Portugal with a master's degree in Informatics Security and bachelor in Computer Science. Currently working remotely at OpenBB."
+      />
+      <meta name="keywords" content="josé, donato, fullstack, openbb" />
+      <meta name="robots" content="index, follow" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="English" />
+      <link rel="stylesheet" href="/styles/index.css" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
 
       <link rel="canonical" href={loc.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
       {head.meta.map((m) => (
         <meta {...m} />
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,153 +2,15 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  const PAST_EXPERIENCE_HOMEPAGE = [
-    {
-      title: "Fullstack Developer",
-      place: "OpenBB",
-      description: (
-        <>
-          Currently working at OpenBB with the mission of building modern
-          investment research platforms for everyone. In this process I've been
-          involved in multiple projects. Built{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://openbb.co"
-            class="underline text-gray-800 hover:text-black"
-          >
-            openbb.co
-          </a>
-          , the company website, using remix.run, TailwindCSS, TypeScript,
-          mdx-bundler. I'm involved in the development of our financial discord
-          and telegram bot made with Python. To manage this bot, built a web
-          platform -{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://my.openbb.co"
-            class="underline text-gray-800 hover:text-black"
-          >
-            my.openbb.co
-          </a>{" "}
-          also using Remix and TailwindCSS. I usually add features (normally
-          crypto related) to our bigger and open source product called{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://openbb.co/github"
-            class="underline text-gray-800 hover:text-black"
-          >
-            OpenBB Terminal
-          </a>
-          .
-        </>
-      ),
-      date: "2021 - Present",
-    },
-    {
-      title: "Cybersecurity Researcher",
-      place: "University of Coimbra",
-      description: (
-        <>
-          As part of my master internship, I was awarded a research scholarship
-          from FCT (the Portuguese public agency that supports science,
-          technology and innovation). This enabled me to work independently
-          while having the support of my supervisor. It was a great challenge
-          that came with a lot of freedom, but also with huge responsibilities.
-          The focus of this internship (and my dissertation) was to assess
-          whether web applications were already a viable alternative to native
-          ones in mobile devices. Based on this, I delivered my master thesis,
-          which was a comparative study between web and native applications,
-          read my{" "}
-          <a
-            target="_blank"
-            aria-label="Master thesis"
-            rel="noopener noreferrer"
-            href="/master-thesis?password=thesis2021"
-            class="underline text-gray-800 hover:text-black"
-          >
-            master thesis here
-          </a>
-          .
-        </>
-      ),
-      date: "2019 - 2020",
-    },
-    {
-      title: "Freelance Fullstack Developer",
-      place: "",
-      description: (
-        <>
-          In 2018, I built the credential platform for Queima das Fitas, one of
-          the biggest festivals in Portugal - it enabled the creation of the
-          credentials and included an application (in Flutter) to read the QR
-          codes at the festivals' entrance. <br /> I have also built a{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://appadvice.com/app/myesjf/1462460664"
-            class="underline text-gray-800 hover:text-black"
-          >
-            native application for a high school in my hometown
-          </a>{" "}
-          and a different{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://noticias.uc.pt/multimedia/videos/queimapp-chegou-a-primeira-aplicacao-mobile-dedicada-a-queima-das-fitas/"
-            class="underline text-gray-800 hover:text-black"
-          >
-            application for Queima das Fitas
-          </a>{" "}
-          which later evolved into{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.facebook.com/EveApp2019/"
-            class="underline text-gray-800 hover:text-black"
-          >
-            EveApp
-          </a>
-          . All these applications were built using React Native. <br />
-          Later, I developed several applications professionally including a
-          website for a nation-wide radio{" "}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://rux.pt"
-            class="underline text-gray-800 hover:text-black"
-          >
-            ruc.pt
-          </a>{" "}
-          using Next.js, TailwindCSS for the frontend and Wordpress, Graphql for
-          the backend, as well as other web applications using Next.js and
-          Firebase for clients in Upwork freelancing platform.
-        </>
-      ),
-      date: "2018 - 2019",
-    },
-  ];
   return (
-    <ul>
-      {PAST_EXPERIENCE_HOMEPAGE.map((experience) => (
-        <li class="mt-6">
-          <h3 class="text-base font-semibold">
-            {experience.title}{" "}
-            {experience.place && (
-              <span class="text-sm font-normal">@ {experience.place}</span>
-            )}
-          </h3>
-          <p class="text-xs mt-1">{experience.date}</p>
-          <p class="text-sm my-2 text-gray-700">{experience.description}</p>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h1>Hello world!</h1>
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "José Donato",
   meta: [
     {
       name: "description",
